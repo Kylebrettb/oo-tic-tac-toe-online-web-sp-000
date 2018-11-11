b08a4cfcbba36b41696a9523ebec3bca6a2b25bf
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-11-11 04:33:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
-    display_board(board)
+    display_@board(board)
   else
     turn(board)
   end
